5030287de58bd217dba8f5ef049ffa541fccf0b1
Anuar Tapia <anuartap@ciencias.unam.mx>
2015-12-15 22:24:11 -0600

    Agrega función isBetween

--- Utils.py
+++ Utils.py
@@ -6,32 +6,51 @@ def magnitude(v):
 	"""
 	Returns the euclidean magnitude of v
 	"""
-    return math.sqrt(sum(v[i]*v[i] for i in range(len(v))))
+	return math.sqrt(sum(v[i]*v[i] for i in range(len(v))))
 
 def add(u, v):
 	"""
 	Returns the vectorial sum of u and v
 	"""
-    return [ u[i]+v[i] for i in range(len(u)) ]
+	return [ u[i]+v[i] for i in range(len(u)) ]
 
 def sub(u, v):
 	"""
 	Returns the vectorial sub of u and v
 	"""
-    return [ u[i]-v[i] for i in range(len(u)) ]
+	return [ u[i]-v[i] for i in range(len(u)) ]
 
 def dot(u, v):
 	"""
 	Returns the vectorial dot product of u and v
 	"""
-    return sum(u[i]*v[i] for i in range(len(u)))
+	return sum(u[i]*v[i] for i in range(len(u)))
 
 def normalize(v):
 	"""
 	Returns the normalized vetor of v
 	"""
-    vmag = magnitude(v)
-    return [ v[i]/vmag  for i in range(len(v)) ]
+	vmag = magnitude(v)
+	return [ v[i]/vmag  for i in range(len(v)) ]
+
+def isBetween(u,v,w):
+	"""
+	Returns true iif w is between u and v in R2
+	ie if w is in the segment formed by u and v
+
+	http://stackoverflow.com/questions/328107/how-can-you-determine-a-point-is-between-two-other-points-on-a-line-segment
+	"""
+	epsilon = 0.001
+	crossproduct = (w[1]-u[1])*(v[0]-u[0]) - (w[0]-u[0])*(v[1]-u[1])
+	if abs(crossproduct) > epsilon : return False
+
+	dotproduct = dot( sub(v,u), sub(w,u) )
+	if dotproduct < 0 : return False
+
+	squaredlengthuv = (v[0]-u[0])*(v[0]-u[0]) + (v[1]-u[1])*(v[1]-u[1])
+	if dotproduct > squaredlengthuv : return False
+
+	return True
 
 if __name__ == '__main__':
     l = [1, 1, 1]
@@ -39,3 +58,8 @@ if __name__ == '__main__':
 
     h = normalize(add(l, v))
     print h
+    a = [1,1]
+    b = [2,2]
+    c = [0.9,0.9]
+    isb = isBetween(a,b,c)
+    print isb
