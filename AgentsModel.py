23a6aa4b037132be217a04b3caf5c441c1072f7c
Anuar Tapia <anuartap@ciencias.unam.mx>
2015-12-16 00:25:33 -0600

    Agrega función visibleBoids y dmax ahora es parámetro de todo el modelo

--- AgentsModel.py
+++ AgentsModel.py
@@ -7,14 +7,16 @@ import random
 class AgentsModel(object):
 	"""
 	numAgents - number of agents
+	dmax - maximal distance Dmax up to which objects can be detected within the boid’s visual hemisphere
 	regularSpeed - regular speed of each agent (U/s)
 	detectionRadius - max radius of view capabilities of each agent (180°)
 	view - max length of view of each agent (1000 U)
 	agility - max degrees of movement capabilities of each agent (30°)
 	"""
-	def __init__(self, numAgents, regularSpeed, detectionRadius, view, agility):
+	def __init__(self, numAgents, dmax, regularSpeed, detectionRadius, view, agility):
 		super(AgentsModel, self).__init__()
 		self.numAgents = numAgents
+		self.dmax = dmax
 		self.regularSpeed = regularSpeed
 		self.detectionRadius = detectionRadius
 		self.view = view
@@ -22,10 +24,10 @@ class AgentsModel(object):
 		# List with all boids in the simulation
 		self.swarm = None
 		self.createSwarm()
-		# Stores boids' position in a given time of the simulation
+		# Matrix that stores boids' position in a given time of the simulation
 		# [i][t] = i-boid's position at t time
 		self.positions = None
-		# Stores boids' orientation in a given time of the simulation
+		# Matrix that stores boids' orientation in a given time of the simulation
 		# [i][t] = i-boid's orientation at t time
 		self.orientations = None
 
@@ -43,11 +45,10 @@ class AgentsModel(object):
 			orientation = normalize((random.random(),random.random()))
 			self.swarm.append(Boid(position,orientation,velocity))
 
-	def simulate(self,steps,dmax,motivation):
+	def simulate(self,steps,motivation):
 		"""
 		Main simulation function, receives:
 		steps - number of time steps
-		dmax - maximal distance Dmax up to which objects can be detected within the boid’s visual hemisphere
 		motivation - motivation parameter dm
 		At end of execution self.positions and self.orientations store the configuration of the
 		system (boids in space) at each step of the simulation
@@ -84,7 +85,9 @@ class AgentsModel(object):
 	
 	def initConfiguration(self,steps):
 		"""
-		Initializes self.positions and self.orientations
+		Initializes self.positions and self.orientations as n*steps matrixes
+		where n=size of swarm and steps=time steps for simulation
+		At time t=0 both matrixes have boids' initial attributes
 		"""
 		n = len(self.swarm)
 		self.positions = [[0 for y in xrange(steps)] for x in xrange(n)]
@@ -93,9 +96,43 @@ class AgentsModel(object):
 			self.positions[i][0] = bi.position
 			self.orientations[i][0] = bi.orientation
 
+	def visibleBoids(self,bi):
+		"""
+		Returns a list of all boids visible to bi in self.swarm
+		"""
+		visibles = []
+		pi = bi.position
+		# Determine the five vectors representing boid's sensors (eyes)
+		# North (straigth) sensor
+		s2 = bi.orientation
+		##ox = bi.orientation[0] # x component of boid's current orientation
+		##oy = bi.orientation[1] # y component of boid's current orientation
+		# West sensor
+		##s0 = (-oy,ox)
+		s0 = (-s2[1],s2[0])
+		# East sensor
+		s4 = (-s0[0],-s0[1])
+		# North-west sensor
+		s1 = normalize(add(s0,s2))
+		# North-east sensor
+		s3 = normalize(add(s2,s4))
+		# Position end point sensors in relative place to boid and extended to maximal distance of detection
+		s0transformed = add(pi, scale(self.dmax,s0))
+		s1transformed = add(pi, scale(self.dmax,s1))
+		s2transformed = add(pi, scale(self.dmax,s2))
+		s3transformed = add(pi, scale(self.dmax,s3))
+		s4transformed = add(pi, scale(self.dmax,s4))
+		# Check all boids in swarm
+		for j, bj in enumerate(self.swarm):
+			pj = bj.position
+			# If bj appears in any of the sensors, append it to visibles
+			if isBetween(pi, s0transformed, pj) or isBetween(pi, s1transformed, pj) or isBetween(pi, s2transformed, pj) or isBetween(pi, s3transformed, pj) or isBetween(pi, s4transformed, pj) :
+				visibles.append(bj)
+		return visibles
+
 
 if __name__ == '__main__':
 	am = AgentsModel(10,1,180,1000,30)
 	print am.swarm
 	for b in am.swarm:
-		print b.orientation, magnitude(b.orientation)
+		print "pos: "+str(b.position)+" ori: "+str(b.orientation)+" mag: "+str(magnitude(b.orientation))
